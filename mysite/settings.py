b54fdb46fe6b7722d74e105c34c9a11e59851d1b
Darian Yane <yanedarian.dev@gmail.com>
2022-11-10 00:40:55 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'BlogListView'
+LOGOUT_REDIRECT_URL = 'BlogListView'
