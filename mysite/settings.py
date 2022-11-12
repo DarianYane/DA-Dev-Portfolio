0c2b2c83685c6b37b50737d4397121c33e09aa74
Darian Yane <darianyane@yahoo.com.ar>
2022-11-12 19:20:46 -0300

    email contact form

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,6 +138,12 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 
 # MEDIA
 MEDIA_URL = "/media/"
