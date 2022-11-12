066835282de8f7d20bbc7db80945c8400ed6a486
Darian Yane <darianyane@yahoo.com.ar>
2022-11-11 21:04:29 -0300

    blog optimized

--- blogusers/forms.py
+++ blogusers/forms.py
@@ -10,6 +10,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
     
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
